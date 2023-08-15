188bfdfd67e6a77e526f37ca144a9bce24967b51
Ryan Buck <ryan@Ryans-MBP.attlocal.net>
2023-08-15 15:39:58 -0500

    Serve hello world with go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"io"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World!\n")
+		io.WriteString(w, r.Method)
+	}
+
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
