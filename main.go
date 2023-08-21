1eb916a3a8e17ce2ceb2f790c799fb674d2a407b
Ryan Buck <buckr56@yahoo.com>
2023-08-21 13:48:54 -0500

    Create data in serve and serve to html

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ import (
 	"html/template"
 )
 
+type Team struct {
+	Name	string
+	ID	string
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		temp := template.Must(template.ParseFiles("index.html"))
-		temp.Execute(w, nil)
+		
+		teams := map[string][]Team{
+			"Teams" : {
+				{Name: "Kowski Krew", ID: "01"},
+				{Name: "JingleHeimerSchmidt", ID:"02"},
+			},
+		}
+		temp.Execute(w, teams)
 	}
 	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe(":8000", nil))
