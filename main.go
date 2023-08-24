ebfda40f38a14bbef156a1c4783a64a502f3ec1c
Ryan Buck <buckr56@yahoo.com>
2023-08-24 15:42:51 -0500

    Get League info and serve

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
@@ -8,9 +9,34 @@ import (
 	"net/http"
 )
 
-type Team struct {
-	Name string
-	ID   string
+type Member struct {
+	DisplayName     string `json:"displayName"`
+	ID              string `json:"id"`
+	IsLeagueManager bool   `json:"isLeagueManager"`
+}
+
+type LeagueAPIResponse []struct {
+	GameID          int      `json:"gameId"`
+	ID              int      `json:"id"`
+	Members         []Member `json:"members"`
+	ScoringPeriodID int      `json:"scoringPeriodId"`
+	SeasonID        int      `json:"seasonId"`
+	SegmentID       int      `json:"segmentId"`
+	Settings        struct {
+		Name string `json:"name"`
+	} `json:"settings"`
+	Status struct {
+		CurrentMatchupPeriod int  `json:"currentMatchupPeriod"`
+		IsActive             bool `json:"isActive"`
+		LatestScoringPeriod  int  `json:"latestScoringPeriod"`
+	} `json:"status"`
+	Teams []struct {
+		Abbrev   string   `json:"abbrev"`
+		ID       int      `json:"id"`
+		Location string   `json:"location"`
+		Nickname string   `json:"nickname"`
+		Owners   []string `json:"owners"`
+	} `json:"teams"`
 }
 
 func main() {
@@ -24,11 +50,12 @@ func main() {
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		temp := template.Must(template.ParseFiles("index.html"))
 
-		res, error := http.Get("https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/1868315?seasonId=2022&view=mTeam")
+		res, error := http.Get("https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/1868315?seasonId=2022")
 		if error != nil {
 			log.Fatal(error)
 		}
 		body, error := io.ReadAll(res.Body)
+
 		res.Body.Close()
 		if res.StatusCode > 299 {
 			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
@@ -36,13 +63,20 @@ func main() {
 		if error != nil {
 			log.Fatal(error)
 		}
-		fmt.Printf("%s", body)
-		teams := map[string][]Team{
-			"Teams": {
-				{Name: "Kowski Krew", ID: "01"},
-				{Name: "JingleHeimerSchmidt", ID: "02"},
-			},
+
+		// parse the JSON Data
+		var leagueAPIResponse LeagueAPIResponse
+		error = json.Unmarshal(body, &leagueAPIResponse)
+		if error != nil {
+			log.Fatal(error)
 		}
+
+		teams := map[string][]Member{
+			"Teams": {{DisplayName: "Kowski Krew", ID: "asdlkfj;as", IsLeagueManager: true}},
+		}
+
+		teams["Teams"] = leagueAPIResponse[0].Members
+
 		temp.Execute(w, teams)
 	}
 
