326914dc0636c619c6ff8e9fe2848aa82fc7a73e
Ryan Buck <buckr56@yahoo.com>
2023-08-26 15:08:17 -0500

    Breakout Type definitions into a separate file

---
+++ apiTypes.go
@@ -0,0 +1,37 @@
+package main
+
+type Member struct {
+	DisplayName     string `json:"displayName"`
+	ID              string `json:"id"`
+	IsLeagueManager bool   `json:"isLeagueManager"`
+}
+
+type Team struct {
+	Abbrev   string   `json:"abbrev"`
+	ID       int      `json:"id"`
+	Location string   `json:"location"`
+	Nickname string   `json:"nickname"`
+	Owners   []string `json:"owners"`
+}
+
+type LeagueStatus struct {
+	CurrentMatchupPeriod int  `json:"currentMatchupPeriod"`
+	IsActive             bool `json:"isActive"`
+	LatestScoringPeriod  int  `json:"latestScoringPeriod"`
+}
+
+type LeagueSettings struct {
+	Name string `json:"name"`
+}
+
+type LeagueAPIResponse []struct {
+	GameID          int            `json:"gameId"`
+	ID              int            `json:"id"`
+	Members         []Member       `json:"members"`
+	ScoringPeriodID int            `json:"scoringPeriodId"`
+	SeasonID        int            `json:"seasonId"`
+	SegmentID       int            `json:"segmentId"`
+	Settings        LeagueSettings `json:"settings"`
+	Status          LeagueStatus   `json:"status"`
+	Teams           []Team         `json:"teams"`
+}
--- main.go
+++ main.go
@@ -9,42 +9,6 @@ import (
 	"net/http"
 )
 
-type Member struct {
-	DisplayName     string `json:"displayName"`
-	ID              string `json:"id"`
-	IsLeagueManager bool   `json:"isLeagueManager"`
-}
-
-type Team struct {
-	Abbrev   string   `json:"abbrev"`
-	ID       int      `json:"id"`
-	Location string   `json:"location"`
-	Nickname string   `json:"nickname"`
-	Owners   []string `json:"owners"`
-}
-
-type LeagueStatus struct {
-	CurrentMatchupPeriod int  `json:"currentMatchupPeriod"`
-	IsActive             bool `json:"isActive"`
-	LatestScoringPeriod  int  `json:"latestScoringPeriod"`
-}
-
-type LeagueSettings struct {
-	Name string `json:"name"`
-}
-
-type LeagueAPIResponse []struct {
-	GameID          int            `json:"gameId"`
-	ID              int            `json:"id"`
-	Members         []Member       `json:"members"`
-	ScoringPeriodID int            `json:"scoringPeriodId"`
-	SeasonID        int            `json:"seasonId"`
-	SegmentID       int            `json:"segmentId"`
-	Settings        LeagueSettings `json:"settings"`
-	Status          LeagueStatus   `json:"status"`
-	Teams           []Team         `json:"teams"`
-}
-
 func main() {
 	fmt.Println("Hello World!")
 
