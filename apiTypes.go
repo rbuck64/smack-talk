1bd3c97dfdad2891b3b0a9aca4897a9bc6493594
Ryan Buck <buckr56@yahoo.com>
2023-09-01 11:35:43 -0500

    Move Types to types file

--- apiTypes.go
+++ apiTypes.go
@@ -35,3 +35,32 @@ type LeagueAPIResponse struct {
 	Status          LeagueStatus   `json:"status"`
 	Teams           []Team         `json:"teams"`
 }
+
+type OpenAICompletionResponse struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Usage   struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+	Choices []struct {
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		FinishReason string `json:"finish_reason"`
+		Index        int    `json:"index"`
+	} `json:"choices"`
+}
+
+type OpenAICompletionRequestBody struct {
+	Model    string `json:"model"`
+	Messages []struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	} `json:"messages"`
+	Temperature float64 `json:"temperature"`
+}
--- main.go
+++ main.go
@@ -10,35 +10,6 @@ import (
 	"net/http"
 )
 
-type OpenAICompletionResponse struct {
-	ID      string `json:"id"`
-	Object  string `json:"object"`
-	Created int    `json:"created"`
-	Model   string `json:"model"`
-	Usage   struct {
-		PromptTokens     int `json:"prompt_tokens"`
-		CompletionTokens int `json:"completion_tokens"`
-		TotalTokens      int `json:"total_tokens"`
-	} `json:"usage"`
-	Choices []struct {
-		Message struct {
-			Role    string `json:"role"`
-			Content string `json:"content"`
-		} `json:"message"`
-		FinishReason string `json:"finish_reason"`
-		Index        int    `json:"index"`
-	} `json:"choices"`
-}
-
-type OpenAICompletionRequestBody struct {
-	Model    string `json:"model"`
-	Messages []struct {
-		Role    string `json:"role"`
-		Content string `json:"content"`
-	} `json:"messages"`
-	Temperature float64 `json:"temperature"`
-}
-
 /*
 curl https://api.openai.com/v1/chat/completions \
   -H "Content-Type: application/json" \
