0e5b8bbc69fa348d93c1155a3cd27e40245622bd
Ryan Buck <buckr56@yahoo.com>
2023-09-01 11:18:05 -0500

    Receive OPENAI Post response and serve to screen

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -8,6 +9,46 @@ import (
 	"log"
 	"net/http"
 )
+
+type OpenAICompletionResponse struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Usage   struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+	Choices []struct {
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		FinishReason string `json:"finish_reason"`
+		Index        int    `json:"index"`
+	} `json:"choices"`
+}
+
+type OpenAICompletionRequestBody struct {
+	Model    string `json:"model"`
+	Messages []struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	} `json:"messages"`
+	Temperature float64 `json:"temperature"`
+}
+
+/*
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer \
+  -d '{
+     "model": "gpt-3.5-turbo",
+     "messages": [{"role": "user", "content": "Say this is a test!"}],
+     "temperature": 0.7
+   }'
+*/
 
 func main() {
 	fmt.Println("Hello World!")
@@ -48,8 +89,63 @@ func main() {
 		teams["Teams"] = leagueAPIResponse.Members
 
 		temp.Execute(w, teams)
+
+		str := getOpenAICall()
+		fmt.Println(str)
+		temp.Execute(w, str)
 	}
 
 	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func getOpenAICall() map[string]string {
+	fmt.Println("Doing the API Call")
+	requestBody := []byte(`{
+		"model": "gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": "Give me a random fact."}],
+		"temperature": 0.7
+	  }`)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", openAIAPIEndpoint, bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+OPEN_AI_KEY)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse the JSON Data
+	var completionResponse OpenAICompletionResponse
+	error := json.Unmarshal(body, &completionResponse)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Println(string(completionResponse.Choices[0].Message.Content))
+
+	completion := map[string]string{
+		"Completion": "Hello There!",
+	}
+
+	completion["Completion"] = completionResponse.Choices[0].Message.Content
+
+	return completion
 }
